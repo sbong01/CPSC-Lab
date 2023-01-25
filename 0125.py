eef8e519b96c5d8d9593b3994cba8b079ba5076e
Saerin Bong <123026544+sbong01@users.noreply.github.com>
2023-01-25 15:57:21 -0500

    Add files via upload

---
+++ 0125.py
@@ -0,0 +1,67 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#the author's name: Saerin Bong
+print("Hello, world")
+Hello, world
+print("Welcome to CPSC 207")
+Welcome to CPSC 207
+print("Hello\nWorld")
+Hello
+World
+print("How\nare\nyou?")
+How
+are
+you?
+print("Today" , "is", "Wednesday")
+Today is Wednesday
+print("Today" + "is" + "Wednesday")
+TodayisWednesday
+print("a\nb\nc")
+a
+b
+c
+print("Hello"+"World")
+HelloWorld
+print("Hello\n"+"\nWorld!")
+Hello
+
+World!
+print("123\n45\n6")
+123
+45
+6
+1+2*3
+7
+(1+2)*3
+9
+7/3
+2.3333333333333335
+7//3
+2
+7%3
+1
+22/4
+5.5
+22//4
+5
+22%4
+2
+3**2
+9
+2**3
+8
+(4+3)//2
+3
+6%2
+0
+
+#Adapt
+1+2*3**4
+163
+
+print("2+2=", 4)
+2+2= 4
+print("50 pound is", 50*0.453592, "Kilograms")
+50 pound is 22.6796 Kilograms
+print("50 pounds is", 50*0.453592, "Kilograms")
+50 pounds is 22.6796 Kilograms
